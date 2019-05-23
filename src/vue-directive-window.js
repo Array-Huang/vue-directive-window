206a3e82ff73ad8cb0f482fe47efb3781ea7a409
Array-Huang <hyw125@gmail.com>
2019-05-23 23:44:11 +0800

    整理拖拽移动的代码

--- src/vue-directive-window.js
+++ src/vue-directive-window.js
@@ -91,6 +91,40 @@ function _handleStartEventForResize(startEvent) {
 }
 
 function _handleStartEventForMove(event) {
+  function _handleEndEventForMove(event) {
+    /* 只有拖拽本体才会挪动，拖拽子元素是不会挪动的 */
+    if (event.target !== event.currentTarget) {
+      return;
+    }
+
+    const el = event.currentTarget; // event.currentTarget是绑定事件的element
+    delete el.dataset.startPoint; // 清除临时值
+    /* 记录transform:translate的值，便于下次拖拽使用 */
+    el.dataset.translate = JSON.stringify(
+      _regexMatchTransform(el.style.transform)
+    );
+    el.removeEventListener(moveEvent, _handleMoveEventForMove, false); // 拖拽结束，清除移动的事件回调
+
+    /* 恢复cursor */
+    el.style.cursor = 'auto';
+
+    event.preventDefault();
+  }
+
+  function _handleMoveEventForMove(event) {
+    const el = event.currentTarget; // event.currentTarget是绑定事件的element
+    const position = _getClientPosition(event); // 获取鼠标/手指的位置
+    const startPoint = JSON.parse(el.dataset.startPoint);
+    const originTranslate = JSON.parse(el.dataset.translate);
+    /* 计算transform:translate的值 */
+    const translate = {
+      x: position.x - startPoint.x + originTranslate.x,
+      y: position.y - startPoint.y + originTranslate.y,
+    };
+
+    el.style.transform = `translate(${translate.x}px, ${translate.y}px)`;
+  }
+
   /* 只有拖拽本体才会挪动，拖拽子元素是不会挪动的 */
   if (event.target !== event.currentTarget) {
     return;
@@ -98,6 +132,7 @@ function _handleStartEventForMove(event) {
   const el = event.currentTarget; // event.currentTarget是绑定事件的element
   el.dataset.startPoint = JSON.stringify(_getClientPosition(event)); // 记录本次拖拽的起点位置
   el.addEventListener(moveEvent, _handleMoveEventForMove, false); // 应在拖拽开始后才绑定移动的事件回调
+  el.addEventListener(endEvent, _handleEndEventForMove);
 
   /* 如果是第一次进行拖拽，则进行初始化：根据transform */
   if (!el.dataset.translate) {
@@ -107,40 +142,6 @@ function _handleStartEventForMove(event) {
 
   /* 调整cursor */
   el.style.cursor = 'all-scroll';
-
-  event.preventDefault();
-}
-
-function _handleMoveEventForMove(event) {
-  const el = event.currentTarget; // event.currentTarget是绑定事件的element
-  const position = _getClientPosition(event); // 获取鼠标/手指的位置
-  const startPoint = JSON.parse(el.dataset.startPoint);
-  const originTranslate = JSON.parse(el.dataset.translate);
-  /* 计算transform:translate的值 */
-  const translate = {
-    x: position.x - startPoint.x + originTranslate.x,
-    y: position.y - startPoint.y + originTranslate.y,
-  };
-
-  el.style.transform = `translate(${translate.x}px, ${translate.y}px)`;
-}
-
-function _handleEndEventForMove(event) {
-  /* 只有拖拽本体才会挪动，拖拽子元素是不会挪动的 */
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-
-  const el = event.currentTarget; // event.currentTarget是绑定事件的element
-  delete el.dataset.startPoint; // 清除临时值
-  /* 记录transform:translate的值，便于下次拖拽使用 */
-  el.dataset.translate = JSON.stringify(
-    _regexMatchTransform(el.style.transform)
-  );
-  el.removeEventListener(moveEvent, _handleMoveEventForMove, false); // 拖拽结束，清除移动的事件回调
-
-  /* 恢复cursor */
-  el.style.cursor = 'auto';
 
   event.preventDefault();
 }
@@ -163,7 +164,6 @@ Vue.directive('window', {
   bind(el) {
     /* 拖拽移动相关 */
     el.addEventListener(startEvent, _handleStartEventForMove);
-    el.addEventListener(endEvent, _handleEndEventForMove);
     /* 拖拽调整大小相关 */
     if (!el.style.position || el.style.position === 'static') {
       el.style.position = 'relative';
