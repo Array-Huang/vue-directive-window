47d50dae9a3b86f94d6fcd08e019f3fbcb166b3f
Array Huang <hyw125@qq.com>
2019-08-05 15:50:24 +0800

    文档导航栏加上github地址、npm地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,9 +5,24 @@ module.exports = {
     ['script', { src: './vue-directive-window.umd.min.js' }],
   ],
   themeConfig: {
+    /* 导航栏链接 */
+    nav: [
+      {
+        text: 'github',
+        link: 'https://github.com/Array-Huang/vue-directive-window',
+      },
+      {
+        text: 'npm',
+        link: 'https://www.npmjs.com/package/vue-directive-window',
+      },
+    ],
+    /* 侧边栏 */
     sidebar: ['/', '/install', '/examples', 'params'],
     sidebarDepth: 2,
+    /* 最后更新时间 */
     lastUpdated: 'Last Updated',
+    /* 活动的标题链接 */
+    activeHeaderLinks: true, // 默认值：true
   },
   chainWebpack(config, isServer) {
     config.resolve.alias.set('@pwd', process.cwd()); // process.cwd() 是当前执行node命令时候的文件夹地址 ——工作目录，保证了文件在不同的目录下执行时，路径始终不变
